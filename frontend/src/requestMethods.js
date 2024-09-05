5b4ccfbb9a35dc09618e0700221e568e3d8d26e9
jashan03 <jashnkh@gmail.com>
2024-09-05 16:13:00 +0530

    updated baseURL

--- frontend/src/requestMethods.js
+++ frontend/src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL ="http://localhost:5000/api/"
+const BASE_URL ="https://e-commerce-2-sigma.vercel.app/api/"
 const TOKEN ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2Yzk1NjM0MWM1ZDRmZjNmYjZlMGYyNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTcyNDgxNTY4MCwiZXhwIjoxNzI1MDc0ODgwfQ.LM4fQoHdLOBftF9m6HcSibG_Pfq-y-dxT-EhF0JE6gU"
 
 // for fetching data
